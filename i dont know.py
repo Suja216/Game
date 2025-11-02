814d66096158777f3dd99e2c8ed300e2ed11113d
Suja216 <chandrasekarans@student.rcdsb.on.ca>
2025-11-02 09:31:08 -0500

    Add files via upload

---
+++ i dont know.py
@@ -0,0 +1,25 @@
+name = input("What's your name? ")
+print("Hello, " + name + "!")
+print ("my name is 36578586997")
+name = input("whats your parents name? ")
+print("thats a cool name for a parent")
+name = input("whats your friends name?")
+print( name +" is a nice name")
+name = input("do you like your friend "+ name)
+if name == 'yes' or 'Yes':
+    print ("cool")
+if name == 'no' :
+    answer = input("why dont you like them if there your friend!?")
+    print("oh thats a good reason")
+name = input("okay are we friends?")
+if name == 'yes' :
+    print("yay!")
+if name == 'no' :
+    print("no problem")
+name = input ("do you like animals?")
+if name == 'yes' :
+    name = input ("what is your favorite animal")
+    print(f"{name} is a nice animal!")
+# Ask a question and get the answer
+answer = input("what is your phone number? ")
+print("mine is " + answer )
